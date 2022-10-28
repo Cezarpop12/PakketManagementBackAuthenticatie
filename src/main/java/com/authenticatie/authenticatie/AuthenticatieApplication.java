70eb7ef2795cf70ec2e36eab0a8f99b7e5a0dce9
CezarPop12 <mr.chaser31@yahoo.com>
2022-10-28 14:12:58 +0200

    Initial commit

---
+++ src/main/java/com/authenticatie/authenticatie/AuthenticatieApplication.java
@@ -0,0 +1,13 @@
+package com.authenticatie.authenticatie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthenticatieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthenticatieApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/authenticatie/authenticatie/AuthenticatieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.authenticatie.authenticatie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthenticatieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
